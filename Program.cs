ba9499aa24e73f1dbdd2c091810ce9aaee570e9e
ahmet ozgun <ozgun_ahmet@hotmail.com>
2021-01-27 11:07:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("A1 Belge", "3");
+            myDictionary.Add("Revize", "4");
+            myDictionary.Add("Yeni Başvuru", "2");
+            myDictionary.Add("Liste", "4");
+            myDictionary.Add("Sertifika", "8");
+
+            myDictionary.ShowAll();
+
+
+        }
+    }
+}
+
